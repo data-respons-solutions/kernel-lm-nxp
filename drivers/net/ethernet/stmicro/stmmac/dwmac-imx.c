af0c626bff95be185cd3700fc0f5bfdc27d0342e
Hans Christian Lonstad <hcl@datarespons.com>
2023-03-28 12:24:39 +0200

    drivers/net/ethernet/dwmac-imx: Add phy supply
    Optional power supply for phy

--- drivers/net/ethernet/stmicro/stmmac/dwmac-imx.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-imx.c
@@ -20,6 +20,7 @@
 #include <linux/regmap.h>
 #include <linux/slab.h>
 #include <linux/stmmac.h>
+#include <linux/regulator/consumer.h>
 
 #include "stmmac_platform.h"
 
@@ -69,7 +70,28 @@ struct imx_priv_data {
 
 	const struct imx_dwmac_ops *ops;
 	struct plat_stmmacenet_data *plat_dat;
+	struct regulator *phy_reg;
 };
+
+static int imx_dwmac_phy_power_on(struct imx_priv_data *bsp_priv, bool enable)
+{
+	int ret = 0;
+
+	if (!bsp_priv->phy_reg)
+		return 0;
+
+	dev_dbg(bsp_priv->dev, "Phy %s\n", enable ? "on" : "off");
+	if (enable) {
+		ret = regulator_enable(bsp_priv->phy_reg);
+		if (ret)
+			dev_err(bsp_priv->dev, "fail to enable phy-supply\n");
+	} else {
+		ret = regulator_disable(bsp_priv->phy_reg);
+		if (ret)
+			dev_err(bsp_priv->dev, "fail to disable phy-supply\n");
+	}
+	return ret;
+}
 
 static int imx8mp_set_intf_mode(struct plat_stmmacenet_data *plat_dat)
 {
@@ -215,7 +237,7 @@ static int imx_dwmac_init(struct platform_device *pdev, void *priv)
 	int ret;
 
 	plat_dat = dwmac->plat_dat;
-
+	imx_dwmac_phy_power_on(priv, true);
 	if (dwmac->ops->set_intf_mode) {
 		ret = dwmac->ops->set_intf_mode(plat_dat);
 		if (ret)
@@ -227,7 +249,7 @@ static int imx_dwmac_init(struct platform_device *pdev, void *priv)
 
 static void imx_dwmac_exit(struct platform_device *pdev, void *priv)
 {
-	/* nothing to do now */
+	imx_dwmac_phy_power_on(priv, false);
 }
 
 static void imx_dwmac_fix_speed(void *priv, unsigned int speed, unsigned int mode)
@@ -360,6 +382,15 @@ imx_dwmac_parse_dt(struct imx_priv_data *dwmac, struct device *dev)
 		}
 	}
 
+	dwmac->phy_reg = devm_regulator_get_optional(dev, "phy");
+	if (IS_ERR(dwmac->phy_reg)) {
+		if (PTR_ERR(dwmac->phy_reg) == -EPROBE_DEFER) {
+			dev_err(dev, "phy regulator is not available yet, deferred probing\n");
+			return -EPROBE_DEFER;
+		}
+		dev_err(dev, "no regulator found\n");
+		dwmac->phy_reg = NULL;
+	}
 	return err;
 }
 
